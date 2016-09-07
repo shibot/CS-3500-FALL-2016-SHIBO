c79a1437da160a63be3366d8d31cba0ee2b2bda3
shibo tang <649077430@qq.com>
2016-09-06 18:48:57 -0600

    git remote add origin https://github.com/uofu-cs3500-fall16/00884092.git

---
+++ PS1/ConsoleApplication1/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaEvaluatorTest
+{
+    class Program
+    {
+        public static int lookUp(string x)
+        {
+            return 4;
+        }
+
+        public static IEnumerable<int> Scramble(int lo, int hi)
+        {
+            if (lo <= hi)
+            {
+                int mid = (lo + hi) / 2;
+                yield return mid;
+                foreach (int n in Scramble(mid + 1, hi))
+                {
+                    yield return n;
+                }
+                foreach (int n in Scramble(lo, mid - 1))
+                {
+                    yield return n;
+                }
+            }
+        }
+        public static int F(ref int x, ref int y)
+        {
+            x = 2;
+            return x + y;
+        }
+        public static int G()
+        {
+            int a = 7;
+            return F(ref a, ref a);
+        }
+        public static void swap(ref int n, ref int m)
+        {
+            int temp = n;
+            n = m;
+            m = temp;
+        }
+
+        public static IEnumerable<int> g()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                yield return i;
+                Console.WriteLine(8 - i);
+            }
+            Console.WriteLine(4);
+        }
+
+        public static void f()
+        {
+            foreach (int n in g())
+            {
+                Console.WriteLine(n);
+                if (n > 1) break;
+            }
+            //Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            //int[] A = { 2, 9 };
+            //Console.WriteLine("Before " + A[0] + " " + A[1]);
+            //swap(ref A[0], ref A[1]);
+            //Console.WriteLine(" After " + A[0] + " " + A[1]);
+            //foreach (int n in Scramble(0, 6))
+            //{
+            //    Console.Write(n);
+            //}
+            //Delcare variable
+            FormulaEvaluator.Evaluator.Lookup lok = lookUp;
+            
+             //String test2 = "2 *3";
+            //string test3 = "2";
+            //string test4 = "A2";
+            //string test5 = "A5 + 8";
+            //string test6 = "3-2";
+            //string test7 = "8/2";
+            //string test8 = "3*5 +8";
+            //string test9 = "3+5*8";
+            //string test10 = "(3+5)*8";
+            //string test11 = "3*(8+5)";
+            //string test12 = "3+(10+8)";
+            //string test13 = "3+(12+3*2)";
+            //string test14 = "3+5*2-(3*4+8)*2+2";
+            //string test15 = "3 * 2 +";
+            //string test16 = "3*(2+3";
+            //string test17 = "3+(5+(9+(2+3)))";
+            //string test18 = "A5 - A5 +A5 *A5/A5";
+            string test19 = "20/0";
+            //string test20 = "-5+3";
+            //string test21 = "";
+
+           //Test1, test expression "5 + 2"
+            try
+            {
+               int test = FormulaEvaluator.Evaluator.Evaluate(test19, lok);
+               Console.WriteLine(test + " First test successful");
+                Console.Read();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("First test failed");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ PS1/FormulaEvaluator/Class1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FormulaEvaluator
+{
+    public static class Evaluator
+    {
+        public delegate int Lookup(String v);
+
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            //create two empty stack, one for values and one for operators.
+            Stack<String> operatorStack = new Stack<String>();
+            Stack<int> valueStack = new Stack<int>();
+            //remove all leading and trailing white-space characters from the current String object.
+            exp = exp.Trim();
+            //split input string into tokens
+            string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+            //check if the String is empty, if it is empty, then throw an exception.
+            if (exp.Equals(""))
+            {
+                throw new ArgumentException("No Expression enter!");
+            }
+            //using For-loop to check all elements in the substring
+            for (int i = 0; i < substrings.Length; i++)
+            {
+                //put the token into a string, use Trim to remove the leading and traling white-space. 
+                String element = substrings[i].Trim();
+                //If the element is a space, then continue to the loop.
+                if (element.Equals(""))
+                {
+                    continue;
+
+                }
+                //If the element is + or - symbol, use the while loop to check whether process operator.
+                else if (element.Equals("+") || element.Equals("-"))
+                {   //If the operator stack is not empty and operator peek element meet one of the symbol,evaluate the value.
+                    while (operatorStack.Count != 0 && (operatorStack.Peek().Equals("+") || operatorStack.Peek().Equals("-")
+                        || operatorStack.Peek().Equals("*") || operatorStack.Peek().Equals("/")))
+                    {
+                        //use helper method valueOperator to evaluate.
+                        Evaluator.valueOperator(valueStack, operatorStack);
+                    }
+                    //push the element into operator stack.
+                    operatorStack.Push(element);
+                }
+                //If the element is * and / symbol,use the while loop to check whether process operator.
+                else if (element.Equals("*") || element.Equals("/"))
+                {
+                    while (operatorStack.Count != 0 && (operatorStack.Peek().Equals("*") || operatorStack.Peek().Equals("/")))
+                    {
+                        Evaluator.valueOperator(valueStack, operatorStack);
+                    }
+                    operatorStack.Push(element);
+                }
+                //If element meet the "(" symbol, simply push it into operatorStack.
+                else if (element.Trim().Equals("("))
+                {
+                    operatorStack.Push("(");
+                }
+                //If the element meet the ")", we need to know if there is "(" symbol on the top of stack.
+                else if (element.Trim().Equals(")"))
+                {
+                    try
+                    {
+                        while (!operatorStack.Peek().Equals("("))
+                        {
+                            Evaluator.valueOperator(valueStack, operatorStack);
+                        }
+                        operatorStack.Pop();
+                    }
+                    catch
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+                //If we meet a token except the "+" "-" "*" "/" "(" ")", we need to try to parse this token to the integer.
+                else
+                {
+                    int value;
+                    //if the value successful parse, push it into the value stack.
+                    if (Int32.TryParse(element, out value))
+                    {
+                        valueStack.Push(value);
+                    }
+                    else
+                    {
+                        //otherwise try to find the value of this element and push the value into valueStack.
+                        try
+                        {
+                            value = variableEvaluator(element);
+                            valueStack.Push(value);
+                        }
+                        catch
+                        {
+                            throw new ArgumentException();
+                        }
+                    }
+                }
+            }
+            while(operatorStack.Count != 0)
+            {
+                if(operatorStack.Count > 0 && valueStack.Count < 2)
+                {
+                    throw new ArgumentException("This expression is invalid");
+                }
+                Evaluator.valueOperator(valueStack, operatorStack);
+            }
+
+            if(valueStack.Count == 1 && operatorStack.Count == 0)
+            {
+                return valueStack.Pop();
+            }
+            else
+            {
+                throw new ArgumentException("Invalid Expression");
+            }
+        }
+
+        public static void valueOperator(Stack<int> valueStack, Stack<string> operatorStack)
+        {
+                String Os = operatorStack.Pop();
+                if (valueStack.Count < 2)
+                    {
+                        throw new ArgumentException();
+                    }
+                //Create two integers and pop the operator stack twice
+                int Op1 = valueStack.Pop();
+                int Op2 = valueStack.Pop();
+                //If the token is "+", then Op2 + Op1
+                if (Os.Equals("+"))
+                {
+                //Push the result into the operand stack
+                    valueStack.Push(Op2 + Op1);
+                }
+                //If the token is "-", then Op2 - Op1
+                else if (Os.Equals("-"))
+                {
+                    //Push the result into the operand stack
+                    valueStack.Push(Op2 - Op1);
+                }
+                //If the token is "/", then Op2 / Op1
+                else if (Os.Equals("/"))
+                {
+                    //If the denominator is zero, then throw an exception
+                    if ((Op1 == 0 && Op2 != 0) || (Op1 != 0 && Op2 == 0))
+                    {
+                        throw new ArgumentException("Can not divided by zero");
+                    }
+                    //If not, push the result into the operand stack
+                    else
+                    {
+                        valueStack.Push(Op2 / Op1);
+                    }
+                }
+                //If the token is "*", then Op2 * Op1
+                else if (Os.Equals("*"))
+                {
+                    //Push the result into the operand stack
+                    valueStack.Push(Op2 * Op1);
+                }
+            }
+    }
+}
